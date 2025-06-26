e58dfac1d280ab22aeb6154678033cc0ee38d51a
joaogarbin <garbinjoaovitor@gmail.com>
2025-06-26 17:20:32 -0300

    first commit

---
+++ tailwind-config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        "primary-black": "#000000",
+        "secondary-black": "#1A1A1A",
+        'primary-red': "#eb0000",
+      },
+    },
+  },
+};
